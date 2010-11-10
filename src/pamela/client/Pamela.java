9beffb154f4275149001859ce18e8aff2f55125c
Jeroen De Dauw <jeroendedauw@gmail.com>
2010-11-10 03:04:52 +0100

    Added code to display a list of items

--- src/pamela/client/Pamela.java
+++ src/pamela/client/Pamela.java
@@ -1,13 +1,71 @@
 package pamela.client;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
-public class Pamela extends Activity {
+public class Pamela extends ListActivity {
+	
+	// ListView that will hold our items references back to main.xml.
+	ListView lstTest;
+	
+	// Array Adapter that will hold our ArrayList and display the items on the ListView.
+	PamelaAdapter arrayAdapter;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        this.showList();
     }
+    
+    protected void showList() {
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.main, this.getListItems()));
+
+        ListView lv = getListView();
+        lv.setTextFilterEnabled(true);
+
+        lv.setOnItemClickListener(new OnItemClickListener() {
+          public void onItemClick(AdapterView<?> parent, View view,
+              int position, long id) {
+            // When clicked, show a toast with the TextView text
+            Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+                Toast.LENGTH_SHORT).show();
+          }
+        });    	
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }   
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+        case R.id.refreshButton:
+        	this.showList();
+        	return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }    
+    
+    protected String[] getListItems() {
+    	return new String[] { Double.toString(Math.random()), Double.toString(Math.random()), Double.toString(Math.random()) };
+    }
+    
 }
